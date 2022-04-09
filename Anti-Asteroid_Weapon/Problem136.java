e7517b845bf9c7edff98f801d792d6b43503614b
Leena Tilwe <leena.tilwe@dowjones.com>
2022-04-09 09:25:58 -0400

    Started Anti-Asteroid problem on Mac

---
+++ Anti-Asteroid_Weapon/Problem136.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Problem136
+{
+	public static void main(String[] args)
+	{
+		try (Scanner input = new Scanner(System.in))
+		{
+			int testCases = Integer.parseInt(input.nextLine());
+			for (int testcase = 0; testcase < testCases; testcase++)
+			{
+				int asteroids = Integer.parseInt(input.nextLine());
+				for (int i=0; i<asteroids; i++)
+				{
+					String[] values = input.nextLine().split(" ");
+					double d = Math.hypot(Integer.parseInt(values[0]), Integer.parseInt(values[1]));
+					System.out.println(d);
+				}
+			}
+		}
+	}
+}
