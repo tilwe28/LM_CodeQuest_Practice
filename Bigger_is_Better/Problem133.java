040503217b49c020ccb029a19341092dbd5fd431
Anurag <anurag.tilwe6@gmail.com>
2022-04-25 11:08:40 -0400

    Bigger is Better

--- Bigger_is_Better/Problem133.java
+++ Bigger_is_Better/Problem133.java
@@ -1,15 +1,15 @@
 import java.util.*;
-public class Problem133
-{
-	public static void main(String[] args)
-	{
-		try (Scanner input = new Scanner(System.in))
-		{
+
+public class Problem133 {
+	public static void main(String[] args) {
+		try (Scanner input = new Scanner(System.in)) {
 			int testCases = Integer.parseInt(input.nextLine());
-			for (int testcase = 0; testcase < testCases; testcase++)
-			{
+			for (int testcase = 0; testcase < testCases; testcase++) {
 				String values = input.nextLine();
-				ArrayList<Integer> nums = new ArrayList<Integer>(Arrays.asList(values.split(" ")));
+				ArrayList<String> strs = new ArrayList<String>(Arrays.asList(values.split(" ")));
+				ArrayList<Integer> nums = new ArrayList<Integer>();
+				for (String s : strs)
+					nums.add(Integer.parseInt(s));
 				System.out.println(Collections.max(nums));
 			}
 		}
